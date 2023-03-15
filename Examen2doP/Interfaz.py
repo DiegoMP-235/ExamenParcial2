88e6c1a39030fd3400b1d2ae1920aed61eede066
DiegoMP-235 <121039161@upq.edu.mx>
2023-03-15 16:53:16 -0600

    Se modifico el archivo de interfaz interfaz
    Se agregaron los widget para poder introducir los datos, una funcion que recupera los datos y manda a llamar al metodo de la clase Alumno para generar la matricula

--- Examen2doP/Interfaz.py
+++ Examen2doP/Interfaz.py
@@ -1,15 +1,62 @@
-from tkinter import Tk,Label,Message,Entry,Button,Frame
+from tkinter import Tk,Label,Entry,Button,Frame,messagebox
 from Alumno import *
 
-nuevoAlumno = Alumno("Isay","GUERRA","LOPEZ","1987","SISTEMAS")
-nuevoAlumno.generaMatricula()
-Matricula = nuevoAlumno.getMatricula()
-print(Matricula)
+def generarMatricula():
+    myMatricula = Alumno(EntradaName.get().upper(),EntradaApellidoP.get().upper(),EntradaApellidoM.get().upper(),EntradaAnioNac.get(),EntradaCarrera.get().upper())
+    myMatricula.generaMatricula()
+    MatriculaNueva = myMatricula.getMatricula()
+    messagebox.showinfo("Matricula","Su matricula es:\n"+MatriculaNueva)
 
-"""
 ventana = Tk()
 ventana.title("Matricula generator")
 ventana.geometry("350x350")
 
-FrameEntradas = Frame()
-"""
+FrameEntradas = Frame(ventana,bg="lightblue")
+FrameBTN = Frame(ventana,bg="grey")
+
+#Titulo
+LabelTittle = Label(FrameEntradas,text="Mi matricula",font=("Arial", 12, "bold"), fg="#333", bg="#f2f2f2", padx=6, pady=6)
+LabelTittle.pack()
+
+#Nombre
+FrameEntradas.pack(expand=True,fill="both")
+FrameBTN.pack(expand=True,fill="both")
+
+NombreLab = Label(FrameEntradas,text="Nombre:")
+EntradaName = Entry(FrameEntradas)
+NombreLab.pack()
+EntradaName.pack()
+
+#Apellidos
+
+ApellidoPLab = Label(FrameEntradas,text="Apellido Paterno:")
+EntradaApellidoP = Entry(FrameEntradas)
+
+ApellidoPLab.pack()
+EntradaApellidoP.pack()
+
+ApellidoMLab = Label(FrameEntradas,text="Apellido Materno:")
+EntradaApellidoM = Entry(FrameEntradas)
+
+ApellidoMLab.pack()
+EntradaApellidoM.pack()
+#Anio nacimiento
+AnioNacLab = Label(FrameEntradas,text="Año de nacimiento:")
+EntradaAnioNac = Entry(FrameEntradas)
+
+AnioNacLab.pack()
+EntradaAnioNac.pack()
+
+#Carrera
+CarreraLab = Label(FrameEntradas,text="Carrera:")
+EntradaCarrera = Entry(FrameEntradas)
+
+CarreraLab.pack()
+EntradaCarrera.pack()
+
+#Botones
+BtnGenerar = Button(FrameBTN,text="Matricula",command=generarMatricula)
+BtnGenerar.pack()
+
+ventana.mainloop()
+
